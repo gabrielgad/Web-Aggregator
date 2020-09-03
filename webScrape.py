8d2263c3d734783abda6842f6c0e4c5bdce7ce12
gabrielgad <59709264+gabrielgad@users.noreply.github.com>
2020-09-03 14:00:05 -0400

    Add files via upload

--- webScrape.py
+++ webScrape.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import urllib.request
 import bs4 as bs
 news = "https://www.breitbart.com/"
@@ -5,6 +6,31 @@ news = "https://www.breitbart.com/"
 newsFile = urllib.request.urlopen(news).read()
 soup = bs.BeautifulSoup(newsFile, 'html.parser')
 
-sauce = soup.find_all(class_="top_article_main")
-print(sauce)
+def mainheadline():
+    sauce = soup.find(class_="top_article_main")
 
+    headline = sauce.h2.a.text
+    desc = sauce.div.p.text
+    print(headline)
+    print(desc)
+    pass
+
+#mainheadline()
+
+def featureheadline():
+    sauce2 = soup.find("section", {"class": "featured_side"})
+    
+    post = sauce2.find_all(class_="post")
+    features = [posts.find("h2").text for posts in post]
+    
+    print(features)
+    pass
+#featureheadline()
+
+headlines = [(mainheadline, featureheadline)]
+labels    = ["Mainheadlines", "Featuredheadlines"]
+news_Info = pd.DataFrame(headlines, columns=labels)
+    
+news_Info.dtypes
+# print(news_Info)
+# news_Info.to_csv('news_Info.csv')
