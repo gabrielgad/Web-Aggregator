c224acdad441bcb24c8cff1db78e17bfa5d83b52
gabrielgad <59709264+gabrielgad@users.noreply.github.com>
2020-09-16 14:23:34 -0400

    Add files via upload
    completed project. Missing timestamps attached to data being harvested.

--- webScrape.py
+++ webScrape.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import urllib.request
 import bs4 as bs
+from openpyxl import load_workbook
 
 news = "https://www.breitbart.com/"
 newsFile = urllib.request.urlopen(news).read()
@@ -11,19 +12,28 @@ sauce = soup.find(class_="top_article_main")
 headline = sauce.h2.a.text
 desc = sauce.div.p.text
 mainheadline = (headline + " | " + desc)
-
+main = pd.Series(mainheadline, name='MainHeadines')
 
 sauce2 = soup.find("section", {"class": "featured_side"})
 post = sauce2.find_all(class_="post")
 features = [posts.find("h2").text for posts in post]
-
+feature = pd.Series(features, name='FeaturedHeadlines')
 
 sauce3 = soup.find("section", {"id": "BBTrendNow"})
 trend = sauce3.find_all('li')
 trending = [trends.find("a").text for trends in trend]
+trends = pd.Series(trending, name='TrendingHeadlines')
 
+df = pd.concat([main,feature,trends], axis=1,)
+#Use this command and comment the one below if you have not initialized the data yet.
+#df.to_excel('Breitbart.xlsx',index=False, header=True)
 
-df = pd.DataFrame(
-    data=(mainheadline, features, trending)
-    )
-df.shape
+book = load_workbook('Breitbart.xlsx')
+writer = pd.ExcelWriter('Breitbart.xlsx', engine='openpyxl')
+writer.book = book
+writer.sheets = {ws.title: ws for ws in book.worksheets}
+
+for sheetname in writer.sheets:
+    df.to_excel(writer,sheet_name=sheetname, startrow=writer.sheets[sheetname].max_row +2, index = False,header= False)
+
+writer.save()
