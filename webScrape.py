2bad3b8909900096cc47558f3936f9b283c06f00
gabrielgad <59709264+gabrielgad@users.noreply.github.com>
2020-09-16 14:40:22 -0400

    Add files via upload
    final file with time stamps and a fully functioning excel sheet that can continuously be appended to.

--- webScrape.py
+++ webScrape.py
@@ -2,6 +2,7 @@ import pandas as pd
 import urllib.request
 import bs4 as bs
 from openpyxl import load_workbook
+from pandas import datetime
 
 news = "https://www.breitbart.com/"
 newsFile = urllib.request.urlopen(news).read()
@@ -14,19 +15,23 @@ desc = sauce.div.p.text
 mainheadline = (headline + " | " + desc)
 main = pd.Series(mainheadline, name='MainHeadines')
 
+
 sauce2 = soup.find("section", {"class": "featured_side"})
 post = sauce2.find_all(class_="post")
 features = [posts.find("h2").text for posts in post]
 feature = pd.Series(features, name='FeaturedHeadlines')
+
 
 sauce3 = soup.find("section", {"id": "BBTrendNow"})
 trend = sauce3.find_all('li')
 trending = [trends.find("a").text for trends in trend]
 trends = pd.Series(trending, name='TrendingHeadlines')
 
+
 df = pd.concat([main,feature,trends], axis=1,)
+df.insert(0, 'TimeStamp', pd.datetime.now().replace(microsecond=0))
 #Use this command and comment the one below if you have not initialized the data yet.
-#df.to_excel('Breitbart.xlsx',index=False, header=True)
+# df.to_excel('Breitbart.xlsx',index=False, header=True)
 
 book = load_workbook('Breitbart.xlsx')
 writer = pd.ExcelWriter('Breitbart.xlsx', engine='openpyxl')
@@ -37,3 +42,4 @@ for sheetname in writer.sheets:
     df.to_excel(writer,sheet_name=sheetname, startrow=writer.sheets[sheetname].max_row +2, index = False,header= False)
 
 writer.save()
+df
