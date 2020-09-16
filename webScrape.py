819dd44ecbc2ac194bab188ad8b82608f66ebbc4
gabrielgad <59709264+gabrielgad@users.noreply.github.com>
2020-09-16 09:28:29 -0400

    Add files via upload
    Working features, not functioning DataFrame

--- webScrape.py
+++ webScrape.py
@@ -1,36 +1,29 @@
 import pandas as pd
 import urllib.request
 import bs4 as bs
-news = "https://www.breitbart.com/"
 
+news = "https://www.breitbart.com/"
 newsFile = urllib.request.urlopen(news).read()
 soup = bs.BeautifulSoup(newsFile, 'html.parser')
 
-def mainheadline():
-    sauce = soup.find(class_="top_article_main")
 
-    headline = sauce.h2.a.text
-    desc = sauce.div.p.text
-    print(headline)
-    print(desc)
-    pass
+sauce = soup.find(class_="top_article_main")
+headline = sauce.h2.a.text
+desc = sauce.div.p.text
+mainheadline = (headline + " | " + desc)
 
-#mainheadline()
 
-def featureheadline():
-    sauce2 = soup.find("section", {"class": "featured_side"})
-    
-    post = sauce2.find_all(class_="post")
-    features = [posts.find("h2").text for posts in post]
-    
-    print(features)
-    pass
-#featureheadline()
+sauce2 = soup.find("section", {"class": "featured_side"})
+post = sauce2.find_all(class_="post")
+features = [posts.find("h2").text for posts in post]
 
-headlines = [(mainheadline, featureheadline)]
-labels    = ["Mainheadlines", "Featuredheadlines"]
-news_Info = pd.DataFrame(headlines, columns=labels)
-    
-news_Info.dtypes
-# print(news_Info)
-# news_Info.to_csv('news_Info.csv')
+
+sauce3 = soup.find("section", {"id": "BBTrendNow"})
+trend = sauce3.find_all('li')
+trending = [trends.find("a").text for trends in trend]
+
+
+df = pd.DataFrame(
+    data=(mainheadline, features, trending)
+    )
+df.shape
